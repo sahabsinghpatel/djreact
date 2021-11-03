86452a9a8c90493aa40a215b569cf36ad62a49ce
sahabsinghpatel <91119274+sahabsinghpatel@users.noreply.github.com>
2021-11-03 09:12:07 +0530

    Update settings.py

--- djreact/settings.py
+++ djreact/settings.py
@@ -129,5 +129,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sahabspatel07@gmail.com'
-EMAIL_HOST_PASSWORD = 'Phoenix&Sahab@1'
+EMAIL_HOST_USER = 'Your Email'
+EMAIL_HOST_PASSWORD = 'Your EMAIL PASSWORD'
